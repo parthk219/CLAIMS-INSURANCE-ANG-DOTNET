13c79323b2696eebcf19221f1c9306fa3d109624
parthk219 <129992000+parthk219@users.noreply.github.com>
2025-12-29 20:29:03 +0530

    routh gaurd

--- insurance-claims-portal/src/app/app.routes.ts
+++ insurance-claims-portal/src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { ClaimsDashboardComponent } from './components/claims/claims-dashboard/claims-dashboard.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
+
   { path: 'login', component: LoginComponent },
-  { path: 'claims', component: ClaimsDashboardComponent },
+
+  {
+    path: 'claims',
+    component: ClaimsDashboardComponent,
+    canActivate: [authGuard]   // 🔐 THIS WAS MISSING
+  },
 ];
